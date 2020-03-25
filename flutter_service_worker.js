f1ed41b471df27131af3b35ea9911a0127a4570d
jungwoos <jungwoos@users.noreply.github.com>
2020-03-26 02:11:03 +0900

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -1,16 +1,13 @@
 'use strict';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "index.html": "65096baf98b3ecc3dc21488068b1d733",
-"main.dart.js": "63fbdc8ddec8b8e3c50d836dcc419dfb",
-"assets/LICENSE": "026bc8c393670bd5400a3192fb81a0f0",
-"assets/AssetManifest.json": "b4d8bfd922da5c9dc6b7aa290d11680a",
-"assets/FontManifest.json": "b3d1c9ea0c36fce7b56886e3213c291d",
-"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "9a62a954b81a1ad45a58b9bcea89b50b",
-"assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
-"assets/assets/logo.png": "e92d75da68b0db510c0ce50855b2de47",
-"assets/assets/fonts/OpenSans-ExtraBold.ttf": "fb7e3a294cb07a54605a8bb27f0cd528",
-"assets/assets/fonts/OpenSans-Regular.ttf": "3ed9575dcc488c3e3a5bd66620bdf5a4"
+  "index.html": "cfa5cff37f2bb2db96f7c8a90134c3a0",
+"main.dart.js": "c0ff445dc63d3f82c5a45ccfac16a215",
+"assets/LICENSE": "4261ac07ca3397d9ea5e04b6f0e0ede8",
+"assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
+"assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
+"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
+"assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16"
 };
 
 self.addEventListener('activate', function (event) {
